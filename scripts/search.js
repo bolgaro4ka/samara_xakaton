9a6488fc655536e334c5cba010063f42f796cda1
Nikita <fedosovnikita007@gmail.com>
2023-12-04 23:29:47 +0300

    Add files via upload

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,3 @@
-const btn=document.getElementsByName("letsGoSearch")[0]
-                const field=document.getElementsByName("searchField")[0]
-                btn.addEventListener("click", (e)=>{window.find(field.value); console.log(field.value)})
+const btn=document.getElementById("letsGoSearch")
+const field=document.getElementsByName("searchField")[0]
+btn.addEventListener("click", (e)=>{window.find(field.value); console.log(field.value)})
